33db0d95f0486dbcba7e39aa8a43c270b78110d0
eskorobogatov <egod09080908@yandex.ru>
2025-03-14 16:03:35 +0300

    keyboard teleop works

--- GTO_control/keyboard_teleop.py
+++ GTO_control/keyboard_teleop.py
@@ -1,14 +1,15 @@
 import numpy as np
+import argparse
 from getch import getch
 from testing import smooth_bringup
 
-from arm_ik import ArmKinematics
+from arm_kinematics import ArmKinematics
 from synchronous_controller import SynchronousController
 from arm_definitions import G1JointLeftArmIndex, G1JointRightArmIndex
 
 class KeyboardTeleop:
 
-    def __init__(self, is_local=False, network_interface=None, is_left=True, dpos=0.01):
+    def __init__(self, is_local=False, network_interface=None, is_left=True, dpos=0.005):
         self.dpos = dpos
 
         # crate stuff for running that shit
@@ -25,6 +26,7 @@ class KeyboardTeleop:
         print("initializing poses")
         joint_states = np.array(self.controller.GetJointStates())
         left_pose, right_pose = self.ik_solver.get_forward_kinematics(joint_states)
+        print('poses intialized')
 
         if is_left:
             self.main_pose = left_pose
@@ -32,6 +34,8 @@ class KeyboardTeleop:
         else:
             self.main_pose = right_pose
             self.off_pose = left_pose
+
+        self.update()
 
         # create keymap
         self.keymap = {
@@ -46,35 +50,39 @@ class KeyboardTeleop:
 
     def update(self):
         new_q = self.ik_solver.solve_ik(
-            l_wrist_target=self.main_pose.homogenous,
-            r_wrist_target=self.off_pose.homogenous
+            l_wrist_target=self.main_pose.homogeneous,
+            r_wrist_target=self.off_pose.homogeneous
         )
-
         msg = {}
 
         for idx, joint in enumerate(G1JointLeftArmIndex):
-            msg[joint] = new_q[idx]
+            msg[joint] = (new_q[idx], 0)
         
         for idx, joint in enumerate(G1JointRightArmIndex):
-            msg[joint] = new_q[7 + idx]
+            msg[joint] = (new_q[7 + idx], 0)
 
         self.controller.ExecuteCommand(msg)
 
 
     # arm actions
     def up(self):
+        print("up")
         self.main_pose.translation[2] += self.dpos
 
     def down(self):
+        print("down")
         self.main_pose.translation[2] -= self.dpos
 
     def left(self):
+        print("left")
         self.main_pose.translation[1] += self.dpos
 
     def right(self):
+        print("right")
         self.main_pose.translation[1] -= self.dpos
 
     def forward(self):
+        print("fwd")
         self.main_pose.translation[0] += self.dpos
 
     def backward(self):
@@ -84,14 +92,24 @@ class KeyboardTeleop:
         pass
 
     def run_teleop(self):
+        print("started teleop")
         cur_cmd = '~'
         while cur_cmd != 'q':
             cur_cmd = getch()
             self.keymap[cur_cmd]()
+            self.update()
             
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--local', help="specifies whether robot is being run in a simulator", action='store_true')
+    parser.add_argument('-ni', '--network-interface', help='network interface for control output', required=False)
+    args = parser.parse_args()
+    return args
+
 def main():
-    tele = KeyboardTeleop(is_local=True)
+    args = parse_args()
+    tele = KeyboardTeleop(is_local=args.local, network_interface=args.network_interface)
     tele.run_teleop()
 
 if __name__ == '__main__':
